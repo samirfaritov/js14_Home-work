6a8248ace8dbc9944872e739c087d10e6970a069
samirfaritov <samirfaritov6@gmail.com>
2023-10-21 15:22:08 +0500

    js_14_Home work

---
+++ main.js
@@ -0,0 +1,35 @@
+// let a = prompt("age")
+
+//     function age() {
+//         if (a.trim() !== '' ) {
+//             if(a.trim() > 18){
+//                 console.log("Welcome");
+//             }
+//             if(a.trim() < 18){
+//                 console.log("Xato");
+//             }
+//         } else {
+//             console.log("Дебил");
+//         }
+//     } 
+//     age()
+
+// let a = prompt("A")
+// let b = prompt("B")
+
+//  function matematika() {
+//     if (a.trim() === 0 && b.trim() === 0) {
+//         console.log("Boshqa nomerni kirit");
+//     } else {
+//         console.log(
+//             Number(a) + Number(b),
+//             Number(a) - Number(b),
+//             Number(a) / Number(b),
+//             Number(a) * Number(b),
+//             Number(a) ** Number(b),
+//             Number(a) % Number(b)
+//         );
+//     }
+//  }
+// matematika()
+ 
